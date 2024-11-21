f37692f3337144c706112ccaf2c5c17907037df1
Rodrigojaure92 <rodrigojauregui894@gmail.com>
2024-11-21 09:50:27 -0500

    subida dia10

--- Day10-Physical/peso.R
+++ Day10-Physical/peso.R
@@ -4,14 +4,14 @@ library(ggplot2)
 library(readr)
 
 # Cargar datos desde la URL
-url <- "https://github.com/ricardoramos12/30daychartchallenge/raw/refs/heads/main/Day10-physical/data.csv"
+url <- "https://github.com/Rodrigojaure92/30daychartchallenge/raw/refs/heads/main/Day10-Physical/data.csv"
 data <- read_csv(url, show_col_types = FALSE)
 
 # Gráfico de densidad de estatura por deporte
-ggplot(data, aes(x = Height, fill = Sport)) +
+ggplot(data, aes(x = Weight, fill = Sport)) +
   geom_density(alpha = 0.6) +
-  labs(title = "Distribución de Estatura de Atletas en Diferentes Deportes",
+  labs(title = "Distribución de Peso de Atletas en Diferentes Deportes",
        x = "Estatura (cm)", y = "Densidad") +
-  scale_fill_manual(values = c("skyblue", "orange", "green")) +
+  scale_fill_manual(values = c("green", "red", "orange")) +
   theme_minimal()
 
