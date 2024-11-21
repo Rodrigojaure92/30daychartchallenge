76876969dfbf9caca416cda280dcd3178df9e7dc
Rodrigojaure92 <rodrigojauregui894@gmail.com>
2024-11-21 10:10:56 -0500

    subida dia 11

--- Day11-mobile-friendly/código.R
+++ Day11-mobile-friendly/código.R
@@ -1,23 +1,24 @@
 # Instalar y cargar las bibliotecas necesarias
 if (!require(tidyverse)) install.packages("tidyverse")
 library(tidyverse)
+library(dplyr)
 
-url2 <- "https://raw.githubusercontent.com/ricardoramos12/30daychartchallenge/refs/heads/main/Day11-mobile-friendly/data.csv"
+url2 <- "https://github.com/Rodrigojaure92/30daychartchallenge/raw/refs/heads/main/Day11-mobile-friendly/dataset.csv"
 data <- read.csv(url2,sep = ",")
+data <- data %>% filter(tipo_app != "Mensajería")
 
 # Resumir la cantidad total de sesiones por hora y tipo de aplicación
 data_summary <- data %>%
   group_by(hora, tipo_app) %>%
   summarise(total_sesiones = sum(sesiones), .groups = "drop")
-
-# Graficar la distribución de uso de aplicaciones móviles a lo largo del día
 ggplot(data_summary, aes(x = hora, y = total_sesiones, color = tipo_app)) +
   geom_line(size = 1.2) +
   labs(
-    title = "Distribución de Uso de Aplicaciones Móviles a lo Largo del Día",
-    x = "Hora del Día",
+    title = "Distribución de manejo de Apps Móviles a lo Largo del Día",
+    x = "Hora",
     y = "Número Total de Sesiones",
     color = "Tipo de Aplicación"
   ) +
   theme_minimal()
+
 
